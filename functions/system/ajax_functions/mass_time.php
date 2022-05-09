b2dbd42747df4f38855a9d575576432c17118787
Samuel <manjigs@gmail.com>
2022-05-09 16:29:40 +0100

    mass_time jquery fix

--- functions/system/ajax_functions/mass_time.php
+++ functions/system/ajax_functions/mass_time.php
@@ -4,13 +4,9 @@ session_start();
 
 
 if (isset($_POST['day'])) {
+
     if ($_POST['day'] == "Weekdays") {
 ?>
-        <div></div>
-    <?php
-    }
-    if ($_POST['day'] == "Weekdays") {
-    ?>
         <div class="form-group">
             <label for="">Time</label>
             <select name="time" id="time" class="form-control" required>
@@ -18,20 +14,26 @@ if (isset($_POST['day'])) {
                 <option value="Evening">Evening</option>
             </select>
         </div>
-    <?php
+        <?php
     } else {
-    ?>
-        <div class="form-group">
-            <label for="">Time</label>
-            <select name="time" id="time" class="form-control" required>
-                <option value="6am">6am</option>
-                <option value="8am">8am</option>
-                <option value="10am">10am</option>
-                <option value="9pm">9pm</option>
-            </select>
-        </div>
+        if ($_POST['day'] == "Weekdays") {
+        ?>
+            <div></div>
+        <?php
+        } else {
+        ?>
+            <div class="form-group">
+                <label for="">Time</label>
+                <select name="time" id="time" class="form-control" required>
+                    <option value="6am">6am</option>
+                    <option value="8am">8am</option>
+                    <option value="10am">10am</option>
+                    <option value="9pm">9pm</option>
+                </select>
+            </div>
 
 <?php
+        }
     }
 }
 
