5bcae0f5aa92c61db34196f70375e76df2e353d0
Capt-Obvs <ogbuknonso@gmail.com>
2022-07-14 11:50:26 +0100

    Index Page (Adjustments)
    Dashboard view arrangements for both Admin & Member

--- index.php
+++ index.php
@@ -24,9 +24,29 @@ $usertype = "damad";
         $Donation = sumAmount('amount', 'payment', ['payment_type' => 'Donation', 'profile_id' => $profile_id]);
         $findSacramentReceived = countRecordsWhere('sacraments_recieved', $profile_id, 'profile_id');
         $societies = countRecordsWhere('organization_has_profile', $profile_id, 'profile_id');
+        $findChurchSacraments = countRecords('sacraments_recieved');
         // ddA($Donation);
         if ($user_type == "admin") {
         ?>
+
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-primary shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                    Total Transactions</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">NGN <?php echo number_format($transactions['SUM(amount)'], 2)
+                                                                                        ?></div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
             <!-- Earnings (Monthly) Card Example -->
             <div class="col-xl-3 col-md-6 mb-4">
@@ -73,8 +93,8 @@ $usertype = "damad";
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
                                 <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                    Total Transactions</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">NGN <?php echo number_format($transactions['SUM(amount)'], 2)
+                                    Total Masses Booked</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800"> <?php echo number_format($massesBooked)
                                                                                         ?></div>
                             </div>
                             <div class="col-auto">
@@ -92,8 +112,8 @@ $usertype = "damad";
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
                                 <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                    Total Masses Booked</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800"> <?php echo number_format($massesBooked)
+                                    Total Sacraments Given</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800"> <?php echo number_format($findChurchSacraments)
                                                                                         ?></div>
                             </div>
                             <div class="col-auto">
