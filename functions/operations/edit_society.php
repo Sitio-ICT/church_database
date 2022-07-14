6ee7e87ce0871a622ae900189223780be1289083
Samuel <manjigs@gmail.com>
2022-07-14 14:29:13 +0100

    Edit Society

---
+++ functions/operations/edit_society.php
@@ -0,0 +1,30 @@
+<?php
+
+include("../methods.php");
+session_start();
+
+if (isset($_POST['id']) && isset($_POST['org_name']) && isset($_POST['meeting_days']) && isset($_POST['org_type']) && isset($_POST['re_occurence'])){
+    $org_name = test_input($_POST['org_name']);
+    $description = test_input($_POST['description']);
+    $meeting_days = test_input($_POST['meeting_days']);
+    $org_type = test_input($_POST['org_type']);
+    $re_occurence = test_input($_POST['re_occurence']);
+    $time = test_input($_POST['time']);
+    $id = $_POST['id'];
+
+    $createOrganization = updateOrganization($id, $org_name, $description, $org_type, $meeting_days, $re_occurence, $time);
+    if ($createOrganization['status'] == "success") {
+        $_SESSION["feedback"] = "Organization Successfuly Edited";
+        echo header("Location: ../../society_view.php?view=$id&message0=$randms");
+        exit();
+    } else {
+        $error = $createOrganization['error'];
+        $_SESSION["feedback"] = "Sorry could not edit Organization! - $error";
+        echo header("Location: ../../society_view.php?view=$id&message1=$randms");
+        exit();
+    }
+}else{
+    $_SESSION["feedback"] = "Fill in all required fields";
+    echo header("Location: ../../society_view.php?view=$id&message1=$randms");
+    exit();
+}
